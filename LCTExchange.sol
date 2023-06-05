276796d3b0461cae74041666344f8c3f8efb1bd9
Haider Yaqoob <49457040+haideryaqoob720@users.noreply.github.com>
2023-06-05 17:03:12 +1000

    Update LCTExchange.sol

--- LCTExchange.sol
+++ LCTExchange.sol
@@ -82,10 +82,11 @@ contract LCTExchange is Ownable {
     }
 
     function adminWithdrawToken() external payable onlyOwner {
-        token.transfer(
+        bool sent = token.transfer(
             owner(),
             (token.balanceOf(address(this)) - exchangeStakedTokens)
         );
+        require(sent, "Failed to transfer tokens from vendor to Owner");
     }
 
     function _addStakeholder(address staker) internal returns (uint) {
@@ -101,7 +102,7 @@ contract LCTExchange is Ownable {
         uint32 _timeStaked // timestaked will be in months Ex. 12,6,3,1,2
     ) public {
         // Check that the requested amount of tokens to sell is more than 0
-        require(_amount > 0, "Cannot stake nothing");
+        require(_amount > 100, "Cannot stake Belown then 100 wei");
 
         // Check that the user's token balance is enough to do the swap
         uint256 userBalance = token.balanceOf(msg.sender);
@@ -136,22 +137,20 @@ contract LCTExchange is Ownable {
         emit Staked(msg.sender, _amount, index, block.timestamp);
     }
 
-    function help_withdrawCheckTimePassedOrNot(
+    function __help_withdrawCheckTimePassedOrNot(
         uint256 _currentTimeStakedSince,
         uint32 stakedTimePeriod
     ) private view returns (bool) {
-        //Example: 1677136837   >  (1674458437    +       (2678400 * 12))
+        //Example: 1677136837   >  (1674458437    +       (2629756 * 12))
 
         bool answer;
-        if (_currentTimeStakedSince == stakedTimePeriod) {
-            if (
-                block.timestamp >
-                _currentTimeStakedSince + (2678400 * stakedTimePeriod)
-            ) {
-                answer = true;
-            } else {
-                answer = false;
-            }
+        if (
+            block.timestamp >
+            _currentTimeStakedSince + (2629756 * stakedTimePeriod)
+        ) {
+            answer = true;
+        } else {
+            answer = false;
         }
         return answer;
     }
@@ -161,7 +160,7 @@ contract LCTExchange is Ownable {
         return user_index;
     }
 
-    function _withdrawStake(uint256 index) public {
+    function withdrawStake(uint256 index) public {
         // require(index < stakes[msg.sender].address_stakes.length, "Invalid stake index.");
 
         uint256 user_index = stakes[msg.sender];
@@ -172,7 +171,7 @@ contract LCTExchange is Ownable {
         ];
         require(current_stake.claimable == true, "You already Withdrew");
         // If we not have to put 2 minutes testing
-        // bool responseTimePassed = help_withdrawCheckTimePassedOrNot(
+        // bool responseTimePassed = _help_withdrawCheckTimePassedOrNot(
         //     current_stake.since,
         //     current_stake.timeStaked
         // );
@@ -185,7 +184,7 @@ contract LCTExchange is Ownable {
         // We also creted two minute stake test this two is two Minute stake test
         // if it is not two minute test then check the time is pass or not
         if (current_stake.timeStaked != 2) {
-            responseTimePassed = help_withdrawCheckTimePassedOrNot(
+            responseTimePassed = _help_withdrawCheckTimePassedOrNot(
                 current_stake.since,
                 current_stake.timeStaked
             );
@@ -200,13 +199,13 @@ contract LCTExchange is Ownable {
         // timeStaked ======= 12, 6, 3, 1, and 2 minute
         uint256 timeDiffrence;
         if (current_stake.timeStaked == 12) {
-            timeDiffrence = 2678400 * 12;
+            timeDiffrence = 2629756 * 12;
         } else if (current_stake.timeStaked == 6) {
-            timeDiffrence = 2678400 * 6;
+            timeDiffrence = 2629756 * 6;
         } else if (current_stake.timeStaked == 3) {
-            timeDiffrence = 2678400 * 3;
+            timeDiffrence = 2629756 * 3;
         } else if (current_stake.timeStaked == 1) {
-            timeDiffrence = 2678400 * 1;
+            timeDiffrence = 2629756 * 1;
         } else if (current_stake.timeStaked == 2) {
             timeDiffrence = 120;
         }
@@ -246,6 +245,7 @@ contract LCTExchange is Ownable {
             token.transfer(msg.sender, incentiveAmount),
             "Transfer failed."
         );
+
         exchangeStakedTokens -= unstakeAmount;
         stakingBalance[msg.sender] -= current_stake.amount;
 
